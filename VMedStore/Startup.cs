5051875b58d6dedcb54c97a638cfbd5f27978fa5
Vinay Kumar Nadiminti <vnaykumar008@gmail.com>
2019-01-19 16:33:56 +0530

    My own medical store startup project

---
+++ VMedStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VMedStore.Startup))]
+namespace VMedStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
